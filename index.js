4d5e304d52c6377c45f90a0d473661cf674aa0b7
Joanna Jacak <joanna.jacak@protonmail.com>
2022-05-20 13:20:59 +0200

    ....

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log(process.env.MONGODB_URI)
 mongoose.connect(
 	process.env.MONGODB_URI
 );
