cc126152669a43f51c1caf32601b81f4d4acb636
Joanna Jacak <joanna.jacak@protonmail.com>
2022-05-20 12:57:52 +0200

    ....

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const {
 const { createCollection, getCollections } = require('./routes/user-routes');
 
 const app = express();
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://collection-manager.netlify.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 
 const corsOptions = {
 	origin: 'https://collection-manager.netlify.app/',
