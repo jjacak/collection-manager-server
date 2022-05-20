77309229f57f88e7f20834b56bb4fb15da0edc6e
Joanna Jacak <joanna.jacak@protonmail.com>
2022-05-20 13:10:35 +0200

    ....

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost:27017/collection-manager'
+	process.env.MONGODB_URI
 );
 
 app.use(getUsers);
